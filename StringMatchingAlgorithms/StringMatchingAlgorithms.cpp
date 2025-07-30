ac73aea5b07dbed912d1dc28f9a02199a7a64774
Raed Salah Bzour <raedbzour77@gmail.com>
2025-07-30 19:17:47 +0300

    adding a reverse string method

--- StringMatchingAlgorithms/StringMatchingAlgorithms.cpp
+++ StringMatchingAlgorithms/StringMatchingAlgorithms.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+std::string reverseString(std::string word)
+{
+    std::string reversed = "";
+    for (int i{};i < word.length();i++)
+    {
+        reversed = word[i] + reversed;
+    }
+    return reversed;
+}
 int main()
 {
+    std::cout<<reverseString("raed");
     std::cin.get();
 }
